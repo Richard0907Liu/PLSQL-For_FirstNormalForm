8e960cc2676a31ab5ebf948bc1c9bcb6026a9dff
Richard0907Liu <51534318+Richard0907Liu@users.noreply.github.com>
2019-06-17 22:10:45 -0400

    Update CS632, HW2.sql

--- CS632, HW2.sql
+++ CS632, HW2.sql
@@ -1,91 +1,3 @@
-drop table borough_neigh;
-/
-
--- HW2 begining
-
-select unique dfta.program_borough as Neighborhood, 
-substr(neigh.community_board, 0, instr(community_board, 'CB') -2) as borough  
-from neigh inner join dfta on neigh.neighborhoods 
-like ('%' || dfta.program_borough || '%');
-
-
-
--- list board and neighborhoods
-begin 
-    for counter in (select * from neigh)
-    loop
-        dbms_output.put_line(counter.community_board ||' '||counter.neighborhoods);
-    end loop;
-end;
-/
-
-
---  a) modified above, get more precise name
-
-begin 
-    for counter in (select * from neigh)
-    loop
-        dbms_output.put_line(
-        substr(counter.community_board, 0, 
-            instr(counter.community_board, 'CB')-2)  ||' '||
-        substr(counter.neighborhoods, 0, instr(counter.neighborhoods, ',')-1));  
-    end loop;
-end;
-/
-
-
--- b)
-declare
-    attr1 varchar(20); attr2 varchar(10); attr3 varchar(10);
-    attr4 varchar(10); attr5 varchar(400);
-    cursor test is select * from neigh;
-begin
-    open test;
-    loop
-        fetch test into attr1, attr2, attr3, attr4, attr5;
-        if test%found
-        then      
-            --dbms_output.put_line(attr1 || ' '|| attr2 || ' ' || attr3 || ' ' 
-            --                    || ' ' || attr4 || ' ' || attr5);
-            --if attr1 = 'Bronx CB 5'  -- hint if want to do all bourogh
-            --then 
-                --dbms_output.put_line(attr1 || ' '|| attr2 || ' ' || attr3 || ' ' 
-                --                      || ' ' || attr4 || ' ' || attr5);
-                attr1 := substr(attr1, 0, instr(attr1, 'CB')-2);
-                split_neighborhoods(attr1, attr5);
-            --else 
-                --dbms_output.put_line('Nothing');
-            --end if;
-        else 
-            exit;
-        end if;
-    end loop;
-    close test;
-end;
-/
-
-
--- c)
-
-declare
-    attr1 varchar(20); attr2 varchar(10); attr3 varchar(10);
-    attr4 varchar(10); attr5 varchar(400);
-    cursor test is select * from neigh;
-begin
-    open test;
-    loop
-        fetch test into attr1, attr2, attr3, attr4, attr5;
-        if test%found
-        then      
-                attr1 := substr(attr1, 0, instr(attr1, 'CB')-2);
-                split_neighborhoods(attr1, attr5);
-        else 
-            exit;
-        end if;
-    end loop;
-    close test;
-end;
-/
 -- create a procedure to process column
 create or replace procedure split_neighborhoods(CB in varchar2, neighborhoods in varchar2)
 as 
@@ -124,6 +36,59 @@ begin
     end loop;
 end;
 /
+                                                       
+-- b)
+declare
+    attr1 varchar(20); attr2 varchar(10); attr3 varchar(10);
+    attr4 varchar(10); attr5 varchar(400);
+    cursor test is select * from neigh;
+begin
+    open test;
+    loop
+        fetch test into attr1, attr2, attr3, attr4, attr5;
+        if test%found
+        then      
+            --dbms_output.put_line(attr1 || ' '|| attr2 || ' ' || attr3 || ' ' 
+            --                    || ' ' || attr4 || ' ' || attr5);
+            --if attr1 = 'BronxÂ CBÂ 5'  -- hint if want to do all bourogh
+            --then 
+                --dbms_output.put_line(attr1 || ' '|| attr2 || ' ' || attr3 || ' ' 
+                --                      || ' ' || attr4 || ' ' || attr5);
+                attr1 := substr(attr1, 0, instr(attr1, 'CB')-2);
+                split_neighborhoods(attr1, attr5);
+            --else 
+                --dbms_output.put_line('Nothing');
+            --end if;
+        else 
+            exit;
+        end if;
+    end loop;
+    close test;
+end;
+/
+
+-- c)
+
+declare
+    attr1 varchar(20); attr2 varchar(10); attr3 varchar(10);
+    attr4 varchar(10); attr5 varchar(400);
+    cursor test is select * from neigh;
+begin
+    open test;
+    loop
+        fetch test into attr1, attr2, attr3, attr4, attr5;
+        if test%found
+        then      
+                attr1 := substr(attr1, 0, instr(attr1, 'CB')-2);
+                split_neighborhoods(attr1, attr5);
+        else 
+            exit;
+        end if;
+    end loop;
+    close test;
+end;
+/
+
 
 create table BOROUGH_NEIGH (
     borough varchar(20),
@@ -134,85 +99,3 @@ create table BOROUGH_NEIGH (
 -- e) 
 select borough, neigh from borough_neigh;
 /
-
--- 3) Create a trigger TZNUMBERS
-/*
-create or replace trigger TZNUMBERS 
-before insert on ZNUMBERS
-for each row
-begin
-    dbms_output.put_line(:new.B);
-end;
-/
-*/
-
-
---insert into ZNUMBERS (B) values (51);
---delete znumbers where B= 53;
-
-/*
-declare
-    insert_num number;
-    total_num number;
-begin
-    for insert_num in 51.. 53
-    loop
-        insert into ZNUMBERS (B) values (insert_num);
-        total_num := 0;
-        for counter in (select * from znumbers)
-        loop   
-            total_num := total_num + counter.b;
-        end loop;
-        dbms_output.put_line('Total number of B: ' || total_num);
-    end loop;
-end;
-/
-*/
-
--- 4) 
--- a)
-Create type HORSE as object(
-    Size1 number,
-    Weight number,
-    Color varchar2(30),
-    MaximumSpeed number
-)
-/
--- b)
-create table THEHORSES (
-    AHORSE HORSE
-)
-/
-
--- c)
-
-insert into THEHORSES values(HORSE(63, 800, 'brown', 20));
-insert into THEHORSES values(HORSE(65, 880, 'black', 23));
-insert into THEHORSES values(HORSE(59, 770, 'white', 18));
-
-
-select AHORSE from thehorses a where a.ahorse.Size1 = 63;
-select a.AHORSE.Size1 from thehorses a where a.ahorse.Size1 = 63;
-select a.AHORSE.Weight from thehorses a where a.ahorse.Size1 = 63;
-select a.AHORSE.Color from thehorses a where a.ahorse.Size1 = 63;
-select a.AHORSE.MaximumSpeed from thehorses a where a.ahorse.Size1 = 63;
-
-select AHORSE from thehorses a where a.ahorse.Size1 = 65;
-select a.AHORSE.Size1 from thehorses a where a.ahorse.Size1 = 65;
-select a.AHORSE.Weight from thehorses a where a.ahorse.Size1 = 65;
-select a.AHORSE.Color from thehorses a where a.ahorse.Size1 = 65;
-select a.AHORSE.MaximumSpeed from thehorses a where a.ahorse.Size1 = 65;
-
-select AHORSE from thehorses a where a.ahorse.Size1 = 59;
-select a.AHORSE.Size1 from thehorses a where a.ahorse.Size1 = 59;
-select a.AHORSE.Weight from thehorses a where a.ahorse.Size1 = 59;
-select a.AHORSE.Color from thehorses a where a.ahorse.Size1 = 59;
-select a.AHORSE.MaximumSpeed from thehorses a where a.ahorse.Size1 = 59;
-
---select * from user_objects;
-select a.ahorse from thehorses a ;
-
-select AHORSE from thehorses a where a.ahorse.Size1 = 63;
-select a.AHORSE.Weight from thehorses a where a.ahorse.Size1 = 63;
-select a.AHORSE.Weight from thehorses a;
-
